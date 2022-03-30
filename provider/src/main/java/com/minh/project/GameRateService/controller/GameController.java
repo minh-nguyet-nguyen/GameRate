d7674a11f474b462b3175cc3d1436d471652dddc
Minh Nguyet Nguyen <minh_nguyet.nguyen@yahoo.com>
2022-03-30 09:45:06 -0400

    adding cross origin for angular app in provider

--- provider/src/main/java/com/minh/project/GameRateService/controller/GameController.java
+++ provider/src/main/java/com/minh/project/GameRateService/controller/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.minh.project.GameRateService.model.GameView;
 import com.minh.project.GameRateService.service.GameService;
  
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/game/")
 public class GameController {
 	
