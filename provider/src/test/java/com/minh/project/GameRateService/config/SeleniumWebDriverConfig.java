d88c90e3c4478233259042a981df40a3a94a1a5c
Minh Nguyet Nguyen <minh_nguyet.nguyen@yahoo.com>
2022-03-31 10:21:51 -0400

    moving selenium tests to provider

---
+++ provider/src/test/java/com/minh/project/GameRateService/config/SeleniumWebDriverConfig.java
@@ -0,0 +1,27 @@
+package com.minh.project.GameRateService.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumWebDriverConfig {
+
+    private WebDriver driver;
+
+    public SeleniumWebDriverConfig() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    public void close() {
+        driver.close();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
